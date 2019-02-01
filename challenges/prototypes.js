b41e5f10c6669243f90080a76c807536c241ed33
Mykol Benning <msmykolbenning@gmail.com>
2019-02-01 13:15:31 -0500

    created prototypes

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -26,8 +26,54 @@
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
+//base constructor
+function CuboidMaker(attr) {
+  this.length = attr.length;
+  this.width = attr.width;
+  this.height = attr. height;
+}
+
+//method
+CuboidMaker.prototype.volume = function() {
+  return `${this.length} * ${this.width} * ${this.height}`;
+}
+
+//method
+CuboidMaker.prototype.surfaceArea = function() {
+  return `2* ${this.length} * ${this.width} * ${this.height} + ${this.width} * ${this.height}`;
+};
+
+
+
+//Object 
+
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5,
+});
+
+console.log(cuboid.volume()); //100
+console.log(cuboid.surfaceArea()); // 130
+
+
+
+
+
+
+
+
+
+
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
+
+
+
+
+
 
 
